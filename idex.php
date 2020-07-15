719373174ca32c12733d07a1ced5ed9217675bd7
andy <andy.ukpe@outlook.com>
2020-07-15 17:03:04 +0200

    Tentata creazione eachfor

--- idex.php
+++ idex.php
@@ -6,7 +6,7 @@ solo quando abbiamo terminato la parte PHP -->
 <!-- creare un database domande risposte -->
 <?php
 $array_faq = [
-  "First Faq" =>[
+  [
     "question" => "Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?",
     "answer" => "La recente decisione della Corte di giustizia dell'Unione europea ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.
 
@@ -18,7 +18,7 @@ $array_faq = [
 
                 Quando cerchi un nome, potresti trovare una notifica in cui viene spiegato che i risultati potrebbero essere stati modificati nel rispetto delle leggi europee per la protezione dei dati. Mostriamo questa notifica agli utenti europei quando cercano la maggior parte dei nomi, non soltanto le pagine che sono state soggette a una rimozione.",
   ],
-  "Second Faq" =>[
+  [
     "question" => "Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?",
     "answer" => "Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. Per noi è imperativo offrire un'elevata sicurezza e assicurarti che le tue informazioni sono al sicuro e accessibili quando ne hai bisogno.
 
@@ -28,12 +28,12 @@ $array_faq = [
 
                 Scopri quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo.",
   ],
-  "Third Faq" =>[
+  [
     "question" => "Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?",
     "answer" => "I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi rimuovere risultati di ricerca da Google non consente di rimuovere i contenuti dal Web. Se desideri rimuovere qualcosa dal Web, devi contattare il webmaster del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica. Inoltre, se, ai sensi delle leggi europee per la
                 protezione dei dati, desideri richiedere la rimozione di determinate informazioni su di te visualizzate nei risultati di ricerca di Google, fai clic qui. Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato l'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente, è inoltre possibile visitare la nostra pagina di assistenza per avere ulteriori informazioni.",
   ],
-  "Fourth Faq" =>[
+  [
     "question" => "Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?",
     "answer" => "In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di URL referrer. Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini
                 di ricerca non vengono inviati come parte dell'URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili qui. Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics o un'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all' esatte parole   chiave che hanno determinato il clic su un annuncio.",
@@ -42,3 +42,16 @@ $array_faq = [
  ?>
 
  <!-- Creazione ciclo eachfor per stampare estrapolare le informazioni dal database -->
+<?php  foreach ($array_faq as $array) { ?>
+  <?php foreach ($array as $faq) {?>
+    <!-- estrapolare ogni domanda -->
+    <ul>
+      <li>Domanda: <?php echo $faq['question']?><br>
+          Risposta: <?php echo $faq['answer'] ?></li>
+
+
+    </ul>
+
+
+  <?php } ?>
+<?php } ?>
